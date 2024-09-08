234398fcab0a10d9eb590449b6fe12acaf851236
shaadansari <ansarisaad90@gmail.com>
2024-09-08 14:38:33 +0530

    update about section

--- components/about.tsx
+++ components/about.tsx
@@ -18,32 +18,22 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
+
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        👋 Hi, I’m <span className="font-medium">Saad Ansari</span>, a <span className="font-medium">React Developer</span> passionate about crafting seamless user experiences and solving complex problems. Currently, I’m an <span className="font-medium">Associate Software Developer</span> at <span className="font-medium">Appstone Pvt Ltd</span>, where I enhanced the PDF generation process for the <span className="font-medium">ImGrows app</span>, reducing load times from an hour to under 2 minutes. 🚀
+      </p>
+
+      <p className="mb-3">
+        My expertise spans <span className="font-medium">React.js, Next.js, JavaScript, CSS, HTML, Node.js, Express.js, Cube.js,</span> and <span className="font-medium">GraphQL</span>, with hands-on experience in <span className="font-medium">microfrontend</span> and <span className="font-medium">PDF rendering</span>. I hold an <span className="font-medium">Master of Engineering</span> from <span className="font-medium">J.T. Mahajan College of Engineering</span>.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        Outside work, I enjoy <span className="font-medium">traveling</span>, <span className="font-medium">playing football</span>, and <span className="font-medium">meditation</span>. I’m always excited to connect with like-minded professionals and explore new opportunities. 🌟 Feel free to reach out or check out my work! 🌐
       </p>
+
+
+
+
     </motion.section>
   );
 }
